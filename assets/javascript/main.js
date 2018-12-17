9416c429c8b8270e113471dea647f2c9aaa8b3f1
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-12-17 08:42:45 -0800

    map

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
   //   //change the size of the logo when user hovers to give the dynamic feel
   //   $(".logo").hover(function(){
   //     //mouse in
@@ -9,33 +8,36 @@ $(document).ready(function(){
   //     $(this).css("width",'100px');
   //  });
 
+  //change the color of up-arrow key at the bottom when user hovers
+  $(".scroll-arrow").hover(
+    function() {
+      //mouse in
+      $(".scroll-arrow").css("color", "black");
+    },
+    function() {
+      //mouse out
+      $(".scroll-arrow").css("color", "rgb(15, 59, 88)");
+    }
+  );
+  //display up arrow key at the bottom only when user scrolls down
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
 
-    //change the color of up-arrow key at the bottom when user hovers
-    $(".scroll-arrow").hover(function(){
-        //mouse in
-       $('.scroll-arrow').css("color",'black');
-    },function(){
-        //mouse out
-        $('.scroll-arrow').css("color",'rgb(15, 59, 88)');
-     });
-    //display up arrow key at the bottom only when user scrolls down
-    $(window).scroll(function() {
-
-
-        var scrollTop = $(this).scrollTop();
-      
-        $('.scroll-arrow').css({
-          opacity: function() {
-            var elementHeight = $(this).height();
-            return 1 - (elementHeight - scrollTop) / elementHeight;
-          }
-        });
-      });
-      $(".scroll").click(function(e) {
-        e.preventDefault();
-        $('body,html').animate({
-          scrollTop: $(this.hash).offset().top
-        }, 1000 );
-      });
-   
+    $(".scroll-arrow").css({
+      opacity: function() {
+        var elementHeight = $(this).height();
+        return 1 - (elementHeight - scrollTop) / elementHeight;
+      }
+    });
+  });
+  $(".scroll").click(function(e) {
+    e.preventDefault();
+    $("body,html").animate(
+      {
+        scrollTop: $(this.hash).offset().top
+      },
+      1000
+    );
+  });
+  $("#location").
 });
